15152b1e168e51729fc5fddcf341d7f2a8a73f8e
TooBoonanek <149961366+TooBoonanek@users.noreply.github.com>
2025-12-19 11:24:21 +0700

    Refactor Cylinder class to use 'length' instead of 'height'

--- hw_06/Python/hw_06/geometry/Cylinder.py
+++ hw_06/Python/hw_06/geometry/Cylinder.py
@@ -1,9 +1,9 @@
 import math
 
 class Cylinder:
-    def __init__(self, radius:float=1.0, height:float=1.0):
+    def __init__(self, radius:float=1.0, length:float=1.0):
         self.__radius = radius
-        self.__height = height
+        self.__length = length
 
     def get_radius(self):
         return self.__radius
@@ -11,17 +11,18 @@ class Cylinder:
     def set_radius(self, radius:float): 
         self.__radius = radius
 
-    def get_height(self):
-        return self.__height
+    def get_length(self):
+        return self.__length
     
-    def set_height(self, height:float):
-        self.__height = height
+    def set_length(self, length:float):
+        self.__length = length
 
     def area(self):
-        return math.pi * pow(self.__radius, 2)
+        return math.pi * pow(self.__radius, 2) * (self.__radius + self.__length)
     
     def volume(self):
-        return self.area() * self.__height
+        return self.area() * self.__length
     
     def cylinder_info(self):
-        return f"Cylinder: Radius = {self.__radius:.2f}, Lenght = {self.__height:.2f}, Area = {self.area():.2f}, Volume = {self.volume():.2f}"
+
+        return f"Cylinder: Radius = {self.__radius:.2f}, Lenght = {self.__length:.2f}, Area = {self.area():.2f}, Volume = {self.volume():.2f}"
